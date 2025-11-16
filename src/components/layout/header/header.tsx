78d468125c00950bab6489609d6d7b51f688d3b1
Artem23112 <artemchernichuk@gmail.com>
2025-11-16 22:17:26 +0200

    Created: Header component

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,32 @@
+import { NavButton } from "@/components/ui/nav-button/nav-button";
+import {
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import clsx from "clsx";
+import s from "./header.module.css";
+
+export const Header = () => {
+  return (
+    <header className={s.header}>
+      <div className={clsx(s.wrapper, "container")}>
+        <nav className={clsx(s.navigation, s.left)}>
+          <NavButton icon={<BurgerIcon type="primary" />}>
+            Конструктор
+          </NavButton>
+          <NavButton icon={<ListIcon type="primary" />}>
+            Лента заказов
+          </NavButton>
+        </nav>
+        <Logo className={clsx(s.center)} />
+        <nav className={clsx(s.navigation, s.right)}>
+          <NavButton icon={<ProfileIcon type="primary" />}>
+            Личный кабинет
+          </NavButton>
+        </nav>
+      </div>
+    </header>
+  );
+};
