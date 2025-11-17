fdf7dd8a319ba8ace75fc10d0c5fec6918777f0e
Artem23112 <artemchernichuk@gmail.com>
2025-11-17 21:14:58 +0200

    Created: layout of BurgerIngredients component

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,3 +1,23 @@
-export const BurgerIngredients = () => {
-  return null;
+import { ingredientsInfo } from "@/assets/mock/ingredients";
+import { IngredientsCatalog } from "@/components/burger-ingredients/components/ingredients-catalog/ingredients-catalog";
+import { Tabs } from "@/components/burger-ingredients/components/tabs/tabs";
+import { useState } from "react";
+
+type Props = {
+  className?: string;
+};
+
+export const BurgerIngredients: React.FC<Props> = ({ className }) => {
+  const [currentTabId, setCurrentTabId] = useState(1);
+
+  return (
+    <div className={className} style={{ maxWidth: "600px", width: "100%" }}>
+      <Tabs
+        className="mb-10"
+        activeTabId={currentTabId}
+        changeActiveTab={setCurrentTabId}
+      />
+      <IngredientsCatalog ingredientsList={ingredientsInfo} />
+    </div>
+  );
 };
