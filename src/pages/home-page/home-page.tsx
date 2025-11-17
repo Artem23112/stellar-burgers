0c3e87eee520dc80d9c60ee59854e5cc910f7aa6
Artem23112 <artemchernichuk@gmail.com>
2025-11-17 21:15:32 +0200

    Created: basic layout of HomePage component

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,11 +1,17 @@
 import { BurgerConstructor } from "@/components/burger-constructor/burger-constructor";
 import { BurgerIngredients } from "@/components/burger-ingredients/burger-ingredients";
+import s from "./home-page.module.css";
 
 export const HomePage = () => {
   return (
     <main>
-      <BurgerIngredients />
-      <BurgerConstructor />
+      <div className="container">
+        <h2 className="text_type_main-large mb-5">Соберите бургер</h2>
+        <section className={s.burgerAssembly}>
+          <BurgerIngredients />
+          <BurgerConstructor />
+        </section>
+      </div>
     </main>
   );
 };
