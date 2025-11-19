3d3f480bdea9a31c3900a540e65aea3a018779c9
Artem23112 <artemchernichuk@gmail.com>
2025-11-19 20:50:25 +0200

    Removed: React.Fragment from component

--- src/components/ui/modals/ingredient-details-modal/ingredient-details-modal.tsx
+++ src/components/ui/modals/ingredient-details-modal/ingredient-details-modal.tsx
@@ -19,33 +19,31 @@ export const IngredientDetailsModal: FC<PropsT> = ({
 
   return (
     <ModalLayout className={clsx(s.content)} onClose={onClick}>
-      <>
-        <div className={s.header}>
-          <h2 className={"text_type_main-large"}>Детали ингредиента</h2>
-          <button type="button" onClick={onClick}>
-            <CloseIcon type="primary" />
-          </button>
-        </div>
-        <img src={image_large} alt={name} />
-        <ul className={clsx(s.additionalInfo, "text_color_inactive")}>
-          <li>
-            <p>Калории,ккал</p>
-            <span className="text_type_digits-default">{calories}</span>
-          </li>
-          <li>
-            <p>Белки, г</p>
-            <span className="text_type_digits-default">{proteins}</span>
-          </li>
-          <li>
-            <p>Жиры, г</p>
-            <span className="text_type_digits-default">{fat}</span>
-          </li>
-          <li>
-            <p>Углеводы, г</p>
-            <span className="text_type_digits-default">{carbohydrates}</span>
-          </li>
-        </ul>
-      </>
+      <div className={s.header}>
+        <h2 className={"text_type_main-large"}>Детали ингредиента</h2>
+        <button type="button" onClick={onClick}>
+          <CloseIcon type="primary" />
+        </button>
+      </div>
+      <img src={image_large} alt={name} />
+      <ul className={clsx(s.additionalInfo, "text_color_inactive")}>
+        <li>
+          <p>Калории,ккал</p>
+          <span className="text_type_digits-default">{calories}</span>
+        </li>
+        <li>
+          <p>Белки, г</p>
+          <span className="text_type_digits-default">{proteins}</span>
+        </li>
+        <li>
+          <p>Жиры, г</p>
+          <span className="text_type_digits-default">{fat}</span>
+        </li>
+        <li>
+          <p>Углеводы, г</p>
+          <span className="text_type_digits-default">{carbohydrates}</span>
+        </li>
+      </ul>
     </ModalLayout>
   );
 };
